d471ae03e2ad2a00b72af9b67c6fc57193ff2fd9
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-11-26 17:06:49 -0300

    :sparkle: feat: Adiciona as rotas do CRUD de animais

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -3,6 +3,9 @@ import { Login } from "../pages/Login";
 import { Home } from "../pages/Home";
 import { CriarTutor } from "../pages/CriarTutor";
 import { EditarTutor } from "../pages/EditarTutor";
+import { AnimaisTutor } from "../pages/AnimaisTutor";
+import { CriarAnimal } from "../pages/CriarAnimal";
+import { EditarAnimal } from "../pages/EditarAnimal";
 
 export function AppRoutes() {
     return(
@@ -14,6 +17,12 @@ export function AppRoutes() {
             <Route path="/tutor/novo" element={<CriarTutor />} />
 
             <Route path="/tutor/editar/:id" element={<EditarTutor/>} />
+
+            <Route path="/tutor/:id/animais" element={<AnimaisTutor/>} />
+
+            <Route path="/tutor/:id/animal/novo" element={<CriarAnimal/>} />
+
+            <Route path="/animal/editar/:id" element={<EditarAnimal />} />
 
             <Route path="*" element={<Login />} />
         </Routes>
