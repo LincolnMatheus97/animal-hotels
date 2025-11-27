29ae2b9ae5b07fd8921c45e7c17bb4cb1662ddae
LincolnMatheus97 <linconlmatheus25@gmail.com>
2025-11-27 09:56:34 -0300

    :recycle: refactor: Refatora variavel de identificação da PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 const SECRET_KEY = 'segredo-super-secreto-123';
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middlewares do json-server
 server.use(middlewares);
