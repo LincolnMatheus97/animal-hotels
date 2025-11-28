38f9e18f7e9e9960975d9a3be250c259e1bd8b3a
natyyHy <natielegrazielly5@gmail.com>
2025-11-28 16:13:43 -0300

    :boom: fix: mudando url da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    //https://animal-hotels-api.onrender.com
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://animal-hotels-api.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
